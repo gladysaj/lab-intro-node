2bc31f50bb294d131e1f265e45b5df6fa96daaf9
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-05-05 23:31:39 -0500

    Iteration5 done

--- starter-code/index.js
+++ starter-code/index.js
@@ -7,22 +7,38 @@ class SortedList {
   add(item) {
     this.items.push(item);
     this.items.sort((a,b) => a-b);
-    this.items = this.items.length
-    //continue
+    // make sure that the length property gets updated accordingly when new items are added to the list.
+    this.length = this.items.length;
   }
 
-  get(pos) {}
-if() {
-  throw new Error("OutOfBounds");
+  get(pos) {
+    this.items.pos = [pos];
+  }
+  //fix this
+    if(pos > this.items) {
+      throw new Error("OutOfBounds");
 }
 
+  max() {
+   this.items = Math.max(...items);
+  }
+  if( SortedList === null) {
+    throw new Error('EmptySortedList');
+  }
 
-  max() {}
+  min() {
+    this.items = Math.min(...items);
+  }
+  if( SortedList === null) {
+    throw new Error('EmptySortedList');
+  }
 
-  min() {}
+  sum() {
+    this.items.reduce((test) => {
+      //complete
+    }
+  }
 
-  sum() {}
-//reduce
   avg() {}
   //sum
 }
